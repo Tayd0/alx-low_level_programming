fc93784c3f07b7f9994859681da3b8863e1c3447
Thato Mogokotleng <teetomg@gmail.com>
2023-06-16 15:28:46 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 /**
-* main - Entry point
-*
+* main - A program that prints the size of various computer types
 * Return: Always 0 (Success)
 */
-int main (void)
+int main(void)
 {
 	char a;
 	int b;
