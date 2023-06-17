2a9f99335cbf20c23febe1384dc8208b76376916
Thato Mogokotleng <teetomg@gmail.com>
2023-06-17 03:15:50 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
+
 /**
-* main - A program that prints the size of various computer types
+* main - Entry point of the program
+*
+* This function uses printf to print the size of various types on the computer it is compiled and run on.
+*
 * Return: Always 0 (Success)
 */
 int main(void)
